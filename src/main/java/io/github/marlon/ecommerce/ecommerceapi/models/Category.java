d7587d8cd42581e15113780001e2603328a89271
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-05 14:51:41 -0300

    Refactor Category and Product entities to use Lombok annotations and update relationships

--- src/main/java/io/github/marlon/ecommerce/ecommerceapi/models/Category.java
+++ src/main/java/io/github/marlon/ecommerce/ecommerceapi/models/Category.java
@@ -1,16 +1,24 @@
 package io.github.marlon.ecommerce.ecommerceapi.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 @Table(name = "categoria")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
-    @ManyToMany(mappedBy = "category")
+    @ManyToMany(mappedBy = "categories")
     private Product product;
 }
--- src/main/java/io/github/marlon/ecommerce/ecommerceapi/models/Product.java
+++ src/main/java/io/github/marlon/ecommerce/ecommerceapi/models/Product.java
@@ -1,9 +1,20 @@
 package io.github.marlon.ecommerce.ecommerceapi.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 @Entity
 @Table(name = "produtos")
 public class Product {
@@ -12,12 +23,19 @@ public class Product {
     private UUID id;
     private String name;
     private Integer preco;
+
     @ManyToMany
     @JoinTable(
             name = "produto-categoria",
             joinColumns =  @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
-
-    private Category category;
+    private Set<Category> categories;
+    @OneToMany(mappedBy = "product")
+    private Set<OrderItem> orderItem;
+    @CreatedDate
+    private LocalDateTime createAt;
+    @LastModifiedDate
+    private LocalDateTime modifyAt;
 }
+
