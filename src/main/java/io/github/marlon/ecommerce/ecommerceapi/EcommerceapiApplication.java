5a72286c848db2afdb739e0c65e8fcd613019e1b
Marlon Ferreira <mferreirab03@gmail.com>
2025-09-05 14:52:13 -0300

    Enable JPA auditing in the application

--- src/main/java/io/github/marlon/ecommerce/ecommerceapi/EcommerceapiApplication.java
+++ src/main/java/io/github/marlon/ecommerce/ecommerceapi/EcommerceapiApplication.java
@@ -2,8 +2,10 @@ package io.github.marlon.ecommerce.ecommerceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcommerceapiApplication {
 
 	public static void main(String[] args) {
